68aa14a54858f697d160f220540642c678be6e9a
Tanzeel4 <tanzeel.haq.khan@gmail.com>
2024-11-07 16:09:56 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import CardSection from './CardSection.jsx';
+import UperSection from './UperSection.jsx';
+import datab from './Data.jsx';
+
+function App() {
+  const data = datab;
+
+  return (
+    <>
+      <UperSection />
+      <CardSection data={data} />
+    </>
+  );
+}
+
+export default App;
